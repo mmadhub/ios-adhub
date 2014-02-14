6c3fe41b78a372dc8c44f713f5581a261098d6f3
mmadhub <contato.adhub@gmail.com>
2014-02-14 18:34:05 -0200

    Estimote SDK

--- AdHub/AdHub/ViewController.h
+++ AdHub/AdHub/ViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <ESTBeaconManager.h>
+@interface ViewController : UIViewController<ESTBeaconManagerDelegate>
 
-@interface ViewController : UIViewController
 
 @end
